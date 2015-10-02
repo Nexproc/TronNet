5b86703992b7779c12c8f488099d514a60a48a69
Nexproc <ntitcombe@gmail.com>
2015-10-01 18:48:49 -0700

    hotfix

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-//   socket.on('disconnect', function(){
-//     console.log('user disconnected :(');
-//   });
-// });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
@@ -20,4 +13,4 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(port);
+server.listen(port, function(){});
